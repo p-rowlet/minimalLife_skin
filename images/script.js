c969f73557fa3682985845973e08187df9094a34
rowlet <kagechi@naver.com>
2022-02-04 05:27:26 +0900

    사이드바 기능 구현

--- images/script.js
+++ images/script.js
@@ -1,4 +1,19 @@
 const scroll = document.querySelector('.scrolling');
+const menu = document.querySelector('.menubar');
+const nav = document.querySelector('nav');
+const hiddenbar = document.querySelector('.hiddenbar');
+const hiddenbari = document.querySelector('.hiddenbar i')
+const sidebar = document.querySelector('.sidebar');
+const bar = document.querySelectorAll('.bar');
+
+if(window.innerWidth < 1400){
+    sidebar.style.display = 'none';
+    sidebar.classList.add('sidebar_inactive');
+    setTimeout(()=>{
+        sidebar.style.display = 'block';
+    },1800);
+}
+
 
 window.addEventListener('scroll', (e)=>{   
     if(window.outerWidth >= 600 && window.scrollY >= 800){
@@ -10,9 +25,19 @@ window.addEventListener('scroll', (e)=>{
 
 window.addEventListener('resize', e=>{
     if(window.innerWidth >= 1400){
-        nav.style.display = 'block';
+        sidebar.style.display = 'block';
+        sidebar.classList.remove('sidebar_inactive');
+        hiddenbar.classList.remove('hiddenbar_active');
+        nav.classList.remove('active');
     }else{
-        nav.style.display = 'none';
+        sidebar.style.display = 'none';
+        sidebar.classList.add('sidebar_inactive');
+        setTimeout(()=>{
+            sidebar.style.display = 'block';
+        },1500);
+        for(let i=0 ; i<3; i++){
+            bar[i].classList.remove('active');
+        }
     }
 });
 
@@ -20,17 +45,21 @@ scroll.addEventListener('click', e=>{
     window.scrollTo({top:0, left:0, behavior:'smooth'})
 });
 
-const menu = document.querySelector('.menubar');
-const nav = document.querySelector('nav');
-
 menu.addEventListener('click', e=>{
-    if(nav.style.display === 'block'){
-        nav.style.display = 'none';
-    }else{
-        nav.style.display = 'block';
+    sidebar.classList.toggle('sidebar_inactive');
+    nav.classList.toggle('active');
+    for(let i=0 ; i<3; i++){
+        bar[i].classList.toggle('active');
     }
-    
 });
+
+hiddenbar.addEventListener('click', e=>{
+    hiddenbar.classList.toggle('hiddenbar_active');
+    hiddenbari.classList.toggle('fa-chevron-right');
+    sidebar.classList.toggle('sidebar_inactive');
+    nav.classList.toggle('active');
+})
+
 
 function requestSearch(className){
     try{
